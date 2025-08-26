5da502aca80aca96f6f42fa046290493ef49cfaa
Peter Paris <pparis@gmu.edu>
2025-08-26 11:33:29 -0400

    minor edits

--- autograders/010_lab_setting_up/tests/test_subprocess.py
+++ autograders/010_lab_setting_up/tests/test_subprocess.py
@@ -8,21 +8,23 @@ class TestDiff(unittest.TestCase):
     def setUp(self):
         pass
 
-    @weight(1)
+    @weight(10)
     def test_no_args(self):
         """The only test:  just verify the script output"""
-        script = subprocess.Popen(["python3", "my_first_script.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        script = subprocess.Popen(
+            ["python3", "my_first_script.py"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         output = script.stdout.read().strip().decode()
-        
+
         p = re.compile(r"Hello\s*(.*)\s*!")
         m = p.match(output)
 
         self.assertIsNotNone(m, "The expected output is of the form 'Hello YOUR_NAME !")
-        
+
         err = script.stderr.read().strip()
         referenceOutput = b""
         self.assertEqual(err, referenceOutput)
-        
-        script.terminate()
 
-    
+        script.terminate()
